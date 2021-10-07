b5c12c2c9f10155a83942f5cf122ebc2925555a3
Etudiant O'clock <gaetan.cristina@gmail.com>
2021-10-07 11:58:07 +0200

    début 11h50 7/10

--- php/home.php
+++ php/home.php
@@ -8,7 +8,7 @@ include "../php/templates/rightcolumn.tpl.php";
 
             
         <article>
-            <section class="middle_column1">
+            <aside class="middle_column1">
             <div class="container">
             <p>Section Story</p>
                 <div>
@@ -30,11 +30,11 @@ include "../php/templates/rightcolumn.tpl.php";
                     <img class="vignette" src="../img/images (6).jpeg" alt="">
                 </div>
             </div>
-            </section>
-            <section class="middle_column2">
+            </aside>
+            <aside class="middle_column2">
             <p>Section Articles</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, corrupti velit beatae veniam id quod eveniet voluptates consequatur quo similique odio autem excepturi repellat laudantium fugiat non minima laborum cum eligendi? Eum magnam pariatur, blanditiis quod earum vero provident ducimus. Recusandae, est nemo exercitationem expedita, consectetur repudiandae, modi laborum soluta illo neque pariatur repellat quisquam dolorum atque minus et ex ullam provident non numquam corporis. Iusto inventore, voluptatum accusamus tenetur ex reiciendis, tempora a saepe, deleniti amet aperiam vero. Sunt tempore ad, asperiores ipsam maiores totam dolores fugiat adipisci magni ab unde quidem sed eligendi quam, quis illum in eaque.</p>
-            </section>
+            </aside>
         </article>
         </main>
      
